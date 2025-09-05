289e6f975c8cfa8cbf794299415255436af04ca9
Victor Barboza <victor.barboza052008@gmail.com>
2025-09-04 21:54:25 -0300

    Category

---
+++ src/main/java/com/devthrive/course/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.devthrive.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devthrive.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+	
+
+}
