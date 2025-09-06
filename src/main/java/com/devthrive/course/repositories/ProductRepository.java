6041c23049ac9bbd924d918229129965cb0a12ad
Victor Barboza <victor.barboza052008@gmail.com>
2025-09-05 21:59:31 -0300

    Product clear

---
+++ src/main/java/com/devthrive/course/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.devthrive.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devthrive.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+	
+
+}
