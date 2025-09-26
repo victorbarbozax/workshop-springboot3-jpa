1aa2ff2b0e7173fc9579621ebf2e033554ba4e05
Victor Barboza <victor.barboza052008@gmail.com>
2025-09-25 21:45:32 -0300

    Subtotal & Total methods

--- src/main/java/com/devthrive/course/entities/Order.java
+++ src/main/java/com/devthrive/course/entities/Order.java
@@ -105,6 +105,14 @@ public class Order implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
