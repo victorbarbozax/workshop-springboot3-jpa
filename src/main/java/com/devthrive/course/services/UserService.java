956a81ae4a5cf702a6dc419a2402f8ddf423e13c
Victor Barboza <victor.barboza052008@gmail.com>
2025-09-28 20:02:51 -0300

    Exception handling - findById

--- src/main/java/com/devthrive/course/services/UserService.java
+++ src/main/java/com/devthrive/course/services/UserService.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import com.devthrive.course.entities.User;
 import com.devthrive.course.repositories.UserRepository;
+import com.devthrive.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() ->  new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
