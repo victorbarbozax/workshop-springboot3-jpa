cf3520f7decd315fabacb01cf75b92272ddf2200
Victor Barboza <victor.barboza052008@gmail.com>
2025-09-28 18:30:31 -0300

    User Update

--- src/main/java/com/devthrive/course/services/UserService.java
+++ src/main/java/com/devthrive/course/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.devthrive.course.entities.User;
@@ -29,4 +30,17 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {	
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		
+	}
+	private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+	
 }
